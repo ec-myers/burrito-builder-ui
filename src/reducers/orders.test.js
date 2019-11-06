20dcb530079efc861ce3e3968f0d92f6f4a74d6a
“Elyse <elysemyers1@gmail.com>
2019-11-06 10:08:04 -0700

    Adds test to orders reducer to check that case SET_ORDERS will match if the type is correct

--- src/reducers/orders.test.js
+++ src/reducers/orders.test.js
@@ -7,4 +7,37 @@ describe('orders', () => {
 
     expect(result).toEqual(expected);
   });
+
+  it('should match the case SET_ORDERS if type is correct', () => {
+    const initialState = [];
+    const action = {
+      type: 'SET_ORDERS',
+      orders: [
+        {
+        id: 1,
+        name: 'Emmett',
+        ingredients: ['beans', 'cheese']
+        },
+        {
+          id: 2,
+          name: 'Penelope',
+          ingredients: ['chicken', 'cheese']
+        }
+      ]
+    };
+    const result = [
+      {
+      id: 1,
+      name: 'Emmett',
+      ingredients: ['beans', 'cheese']
+      },
+      {
+        id: 2,
+        name: 'Penelope',
+        ingredients: ['chicken', 'cheese']
+      }
+    ];
+
+    expect(orders(initialState, action)).toEqual(result);
+  });
 });
